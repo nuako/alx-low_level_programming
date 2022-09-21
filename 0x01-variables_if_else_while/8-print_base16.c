3d38a6e403ec9a039e814e31ee9793dea048f96c
nuako <nuako11@gmail.com>
2022-09-21 17:40:34 +0000

    a program that prints all the numbers of base 16 in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,7 +4,7 @@
  * main - a program that prints all the numbers of base 16 in lowercase,
  * followed by a new line.
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
